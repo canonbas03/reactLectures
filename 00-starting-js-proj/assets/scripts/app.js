972b59d0440c8afffadbb9e1315bd6ced0a07a04
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-07-29 18:53:35 +0300

    00

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -3,3 +3,27 @@ console.log(apikey);
 
 import * as utils from "./util.js";
 console.log(utils.thirdKey);
+
+// DESTRUCTURING
+const { name: userName, age } = {
+  name: "John",
+  age: 18,
+};
+
+console.log(userName);
+
+// saves a string to the browser, even closing and reopening doesnt get deleted
+function storeOrder({ id, currency }) {
+  localStorage.setItem("id", id);
+  localStorage.setItem("currency", currency);
+}
+
+const order = {
+  id: 123,
+  currency: "BGN",
+};
+storeOrder(order);
+
+// CONTROL STRUCTURES
+const password = prompt("Write your pass");
+console.log(password);
