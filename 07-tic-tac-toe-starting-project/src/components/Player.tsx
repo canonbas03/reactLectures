7628fbd2fa72a1ec6a32c832081fc22f2ddf46b6
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-08-05 16:31:35 +0300

    create Palyer component hardcoded names

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.tsx
@@ -0,0 +1,16 @@
+type PlayerProps = {
+  name: string;
+  symbol: string;
+};
+
+export default function Player({ name, symbol }: PlayerProps) {
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button>Edit</button>
+    </li>
+  );
+}
