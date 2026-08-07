7dffb27bffaf83212ff9107b87514e7ef1aa1088
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-08-07 15:13:44 +0300

    User input add

--- 08-starting-project/src/App.tsx
+++ 08-starting-project/src/App.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 10,
+    duration: 5,
+  });
+  function handleChange(inputId: string, newValue: number) {
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [inputId]: newValue,
+    }));
+  }
   return (
     <>
       <Header />
-      User Input
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
       Dynamic Calculations
     </>
   );
 }
-
 export default App;
--- 08-starting-project/src/components/UserInput.tsx
+++ 08-starting-project/src/components/UserInput.tsx
@@ -1,4 +1,14 @@
-export default function UserInput() {
+import { useState } from "react";
+type UserInputProps = {
+  userInput: {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  };
+  onChange: (inputId: string, newValue: number) => void;
+};
+export default function UserInput({ userInput, onChange }: UserInputProps) {
   return (
     <section id="user-input">
       <div className="input-group">
@@ -6,20 +16,28 @@ export default function UserInput() {
           <label htmlFor="initial-investment">Initial Investment</label>
           <input
             id="initial-investment"
+            value={userInput.initialInvestment}
             type="number"
             step={0.01}
             min={0}
             required
+            onChange={(event) =>
+              onChange("initialInvestment", Number(event.target.value))
+            }
           />
         </p>
         <p>
           <label htmlFor="annual-investment">Annual Investment</label>
           <input
             id="annual-investment"
+            value={userInput.annualInvestment}
             type="number"
             step={0.01}
             min={0}
             required
+            onChange={(event) =>
+              onChange("annualInvestment", Number(event.target.value))
+            }
           />
         </p>
       </div>
@@ -28,15 +46,28 @@ export default function UserInput() {
           <label htmlFor="expected-return">Expected Return</label>
           <input
             id="expected-return"
+            value={userInput.expectedReturn}
             type="number"
             step={0.01}
             min={0}
             required
+            onChange={(event) =>
+              onChange("expectedReturn", Number(event.target.value))
+            }
           />
         </p>
         <p>
           <label htmlFor="duration">Duration</label>
-          <input id="duration" type="number" min={0} required />
+          <input
+            id="duration"
+            value={userInput.duration}
+            type="number"
+            min={0}
+            required
+            onChange={(event) =>
+              onChange("duration", Number(event.target.value))
+            }
+          />
         </p>
       </div>
     </section>
