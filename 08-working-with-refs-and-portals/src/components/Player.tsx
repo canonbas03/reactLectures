182d008248613c5de4df5e54567d5bd6275c557d
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-08-13 13:51:56 +0300

    Update Player.tsx

--- 08-working-with-refs-and-portals/src/components/Player.tsx
+++ 08-working-with-refs-and-portals/src/components/Player.tsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const playerNameRef = useRef<HTMLInputElement>(null);
+  const [playerName, setPlayerName] = useState<string | null>(null);
+  function handleSetPlayerName() {
+    if (playerNameRef.current) {
+      setPlayerName(playerNameRef.current.value);
+      playerNameRef.current.value = "";
+    }
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameRef} type="text" />
+        <button onClick={handleSetPlayerName}>Set Name</button>
       </p>
     </section>
   );
