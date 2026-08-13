2588bcabaed9e2c4e7609383845bee6a2538d12c
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-08-13 11:30:27 +0300

    tailwind styles

--- 06-styling-react-components/src/components/Button.tsx
+++ 06-styling-react-components/src/components/Button.tsx
@@ -1,17 +1,15 @@
-import { styled } from "styled-components";
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+import { ComponentPropsWithoutRef, ReactNode } from "react";
 
-  &:hover {
-    backßground-color: #f0920e;
-  }
-`;
-
-export default Button;
+type ButtonProps = {
+  children: ReactNode;
+} & ComponentPropsWithoutRef<"button">;
+export default function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button
+      className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
