b608f2946871b9e24fc2f9e5292496e987d20956
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-08-17 11:25:46 +0300

    Initial project to tsx setup

---
+++ 09-practice-project-project-management/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold text-red-600">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
