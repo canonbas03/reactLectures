f4b9e922de0d35fe22647fcf3c2894c0b38bc551
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-08-18 11:01:42 +0300

    Initial setup 09-react

---
+++ 09-practice-project-project-management-udemy/src/custom.d.ts
@@ -0,0 +1,12 @@
+// declare module "*.png" {
+//   const value: string;
+//   export default value;
+// }
+
+declare module "*.css";
+
+// You can also add other image formats here if needed:
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.png";
