3e7f2d1ec757c8bc7edb0f14b8f717b98838220d
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-09-01 16:47:44 +0300

    Update AvailablePlaces.tsx

--- 15-http-requests/src/components/AvailablePlaces.tsx
+++ 15-http-requests/src/components/AvailablePlaces.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Place } from "../App.js";
 import Places from "./Places.jsx";
 
@@ -5,7 +6,21 @@ type AvailablePlacesProps = {
   onSelectPlace: (place: Place) => void;
 };
 export default function AvailablePlaces({ onSelectPlace }: AvailablePlacesProps) {
+  const [availablePlaces, setSvailablePlaces] = useState([]);
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setSvailablePlaces(resData.places);
+    });
+
   return (
-    <Places title="Available Places" places={[]} fallbackText="No places available." onSelectPlace={onSelectPlace} />
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
   );
 }
